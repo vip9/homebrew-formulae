c551dfc836bb923d4ed354a0531db37f50740626
vip9 <piotr.budny@gmail.com>
2021-11-02 19:20:08 +0100

    Update arm-none-eabi-gcc.rb

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -5,7 +5,7 @@ class ArmNoneEabiGcc < Formula
   homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
   version '10.3-2021.10'
 
-  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-mac.tar.bz2 '
+  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-mac.tar.bz2'
   sha256 'fb613dacb25149f140f73fe9ff6c380bb43328e6bf813473986e9127e2bc283b'
 
   def install
